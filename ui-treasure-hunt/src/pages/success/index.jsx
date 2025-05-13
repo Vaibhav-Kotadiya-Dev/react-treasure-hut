1157eeb7a49b4ef9313d8b84ba3e7f4f75819102
vishu <vivek4754@gmail.com>
2025-05-13 23:09:07 +0530

    fix: ui text

--- ui-treasure-hunt/src/pages/success/index.jsx
+++ ui-treasure-hunt/src/pages/success/index.jsx
@@ -71,7 +71,7 @@ const PaymentSuccess = () => {
           Payment confirmed!
         </Typography>
         <Typography variant="body1" className="adventure-text">
-         {`Thanks for registering! Please check your WhatsApp — we’ve sent you a confirmation message.`}
+        {`You’ve successfully registered. You’ll receive a confirmation message on WhatsApp either today or on your event day.`}
         </Typography>
         <Button
           color="black"
